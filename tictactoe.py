ec85c58ab7b436878613c6416fae8a6428fa8587
Drew Willey <andwilley@gmail.com>
2020-07-02 15:46:57 -0600

    rename functions

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@ from string import ascii_lowercase
 # Tic Tac Toe
 
 # Create a 3x3 list of blank spaces.
-def new_board(dim):
+def create_new_board(dim):
     return [[None]*dim for _ in range(dim)]
 
 # Print the current board to the terminal.
@@ -18,7 +18,7 @@ def get_player_from_turn(turn):
 
 # Throws ValueError if numbers aren't integers or IOError if they aren't on the
 # board.
-def parse_move(cli_input, board):
+def parse_move_input(cli_input, board):
     cli_input = list(map(lambda x: int(x), cli_input.strip().split(' ')))
     if len(cli_input) != 2 or \
             not (0 <= cli_input[0] < len(board[0])) or \
@@ -26,22 +26,17 @@ def parse_move(cli_input, board):
         raise IOError("fuck off")
     return cli_input[0], cli_input[1]
 
-# Make sure the space is not already taken.
 def square_is_open(move, board):
     return not board[move[0]][move[1]]
 
-def update_board(board, move, player):
-    board[move[0]][move[1]] = player
-    return board
-
-def check_rows(board):
+def check_rows_for_winner(board):
     for row in board:
         if row[0] and len(set(row)) == 1:
             print('row', row[0])
             return row[0]
     return None
 
-def check_diagonals(board):
+def check_diagonals_for_winner(board):
     left_to_right_vals = set()
     right_to_left_vals = set()
     for col, row in enumerate(board):
@@ -53,19 +48,17 @@ def check_diagonals(board):
         return right_to_left_vals.pop()
     return None
 
-# Assumes a square board.
-def get_winner(board):
     # Check the rows for a winner.
-    maybe_winner = check_rows(board)
+    maybe_winner = check_rows_for_winner(board)
     if maybe_winner:
         return maybe_winner
     # Check the columns for a winner.
     transposed_board = map(list, zip(*board))
-    maybe_winner = check_rows(transposed_board)
+    maybe_winner = check_rows_for_winner(transposed_board)
     if maybe_winner:
         return maybe_winner
     # Check the diagonals for a winner.
-    return check_diagonals(board)
+    return check_diagonals_for_winner(board)
 
 # Get a valid move recursively.
 def get_valid_move(player, board):
@@ -74,7 +67,7 @@ def get_valid_move(player, board):
     print()
     move = None
     try:
-        move = parse_move(cli_input, board)
+        move = parse_move_input(cli_input, board)
     except IOError as err:
         print("That's not a space on this board, try again!")
         return get_valid_move(player, board)
@@ -88,7 +81,7 @@ def get_valid_move(player, board):
 def run(dimension = 3):
     turn = 0
     winner = None
-    board = new_board(dimension)
+    board = create_new_board(dimension)
     player = 'X'
 
     print('Welcome to Tic Tac Toe!')
@@ -99,7 +92,7 @@ def run(dimension = 3):
         print()
         player = get_player_from_turn(turn)
         move = get_valid_move(player, board)
-        board = update_board(board, move, player)
+        board[move[0]][move[1]] = player
         print_board(board)
         winner = get_winner(board)
         turn += 1
